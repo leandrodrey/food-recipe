bb7274f9a29c0c8ad673f80db270deceddc47df9
Leandro Rey <leandrodrey@gmail.com>
2023-08-11 22:39:15 -0300

    Database and mongoose connection. API development

--- app/api/recipes/route.jsx
+++ app/api/recipes/route.jsx
@@ -2,7 +2,7 @@ import {NextResponse} from "next/server";
 import {db} from "@/database";
 import {Recipe} from "@/models";
 
-export async function GET(req, res) {
+export async function GET() {
 
     await db.connect();
     const recipes = await Recipe.find().sort({id: 'ascending'});
@@ -16,9 +16,45 @@ export async function GET(req, res) {
     return new NextResponse(JSON.stringify(json_response));
 }
 
+const validateRecipeData = (title, rating, ingredients, steps) => {
+    const errors = [];
+
+    if (!title) {
+        errors.push({
+            message: "El título es obligatorio"
+        });
+    }
+
+    if (rating !== null && !Number.isInteger(rating) && !rating < 0) {
+        errors.push({
+            message: "La calificación debe ser un número entero"
+        });
+    }
+
+    if (!ingredients) {
+        errors.push({
+            message: "Los ingredientes son obligatorios"
+        });
+    }
+
+    if (!steps) {
+        errors.push({
+            message: "Los pasos son obligatorios"
+        });
+    }
+
+    return errors;
+};
+
 export async function POST(req) {
 
-    const {title, rating, ingredients, steps} = await req.json()
+    const {title, rating, ingredients, steps} = await req.json();
+
+    const errors = validateRecipeData(title, rating, ingredients, steps);
+
+    if (errors.length) {
+        return new NextResponse(JSON.stringify({message: "Error"}));
+    }
 
     const newRecipe = new Recipe({
         title,
