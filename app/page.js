e833d8ed22ea113f08d838898b79384e82facb0e
Leandro Rey <leandrodrey@gmail.com>
2023-08-18 16:05:23 -0300

    Added the provider

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
+"use client"
+import {useContext, useEffect} from "react";
 import Link from "next/link";
-import {Search, RecipesTable} from "src/frontend/components";
-
-const recipes = () => {
-}
+import {RecipeContext} from "@/src/frontend/context/RecipeProvider";
+import {Search, RecipesTable} from "@/src/frontend/components";
 
 export default function Home() {
+
+    const {recipes, getRecipes} = useContext(RecipeContext);
+
+    useEffect(() => {
+        getRecipes();
+    }, []);
+
     return (
         <main>
             <Search />
@@ -13,7 +20,7 @@ export default function Home() {
             <Link href={'/rating'}>Rating</Link>
             <br />
             <br />
-            <RecipesTable />
+            <RecipesTable recipes={recipes} />
         </main>
     )
 }
--- src/frontend/components/RecipesTable.jsx
+++ src/frontend/components/RecipesTable.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
-export const RecipesTable = () => {
+export const RecipesTable = ({recipes}) => {
+
     return (
-        <div>RecipesTable</div>
+        <>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Recipe</th>
+                        <th scope="col">Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recipes.map((recipe) => (
+                        <tr key={recipe.id}>
+                            <td>{recipe.title}</td>
+                            <td>{recipe.rating}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
     )
 }
