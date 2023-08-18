a777c3d57cf4e2c249e3e8347d6bf367143626ce
Leandro Rey <leandrodrey@gmail.com>
2023-08-18 16:02:53 -0300

    Name changed

--- src/frontend/components/RecipeTab/RecipeTab.jsx
+++ src/frontend/components/RecipeTab/RecipeTab.jsx
@@ -1,7 +1,7 @@
 'use client';
 import {useState} from "react";
 import {Box, Tab, Tabs} from "@mui/material";
-import {CustomTabPanel} from "@/src/frontend/components/tabs/CustomTabPanel";
+import {CustomTabPanel} from "@/src/frontend/components/RecipeTab/CustomTabPanel";
 
 export const RecipeTab = () => {
 
